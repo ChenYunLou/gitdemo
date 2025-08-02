d9c8ad64f2ac8e9c51de1d504acb0a5b255c8aea
YunLou <chen@yunlou.net>
2025-08-02 10:35:45 +0800

    test test

--- 1.py
+++ 1.py
@@ -1,22 +1,29 @@
-//
-//
-//
-//
 import requests
 import pandas as pd
+import datetime as dt
 import json
-url = "https://api.cnyes.com/media/api/v1/newslist/category/headline"
+data = []
+url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" #新聞連結
 payload = {
-    "page":7,
+    "page":1,
     "limit":30,
     "isCategoryHeadline":1,
-    "startAt":1752646755,
-    "endAt":1753510755}
-res = requests.get(url,params=payload)
-jd=json.loads(res.text)
-df=pd.DataFrame(jd['items']['data'])
-df=df[['newsId','title','summary']]
-df['link'] = df['newsId'].apply(lambda x: 'https://m.cnyes.com/news/id/' + str(x))
-df.to_csv('news.csv',encoding='utf-8-sig')
+    "startAt":int((dt.datetime.today() - dt.timedelta(days = 10)).timestamp()),
+    "endAt":int(dt.datetime.today().timestamp())
+} #參數
+res = requests.get(url, params = payload) #連線鉅亨網
+jd = json.loads(res.text) #解析JSON轉成dict
+data.append(pd.DataFrame(jd['items']['data']))
+
+for i in range(2, jd['items']['last_page'] + 1):
+    print("i = ", i)
+    payload["page"] = i
+    res = requests.get(url, params = payload) #連線鉅亨網
+    jd = json.loads(res.text) #解析JSON轉成dict  
+    data.append(pd.DataFrame(jd['items']['data']))
+    
+df = pd.concat(data, ignore_index = True) #取出新聞資料
+df = df[['newsId', 'title', 'summary']]#取出特定欄位
+df['link'] = df['newsId'].apply(lambda  x: 'https://m.cnyes.com/news/id/' + str(x))#建立連結
+df.to_csv('news.csv', encoding = 'utf-8-sig')
 print(df)
-//
